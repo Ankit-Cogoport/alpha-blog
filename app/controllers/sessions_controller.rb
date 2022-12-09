6449f903a23191f823a2393881e5f9cfaff45ee1
Ankit Anand <ankit.anand@cogoport.com>
2022-12-09 17:22:46 +0530

    login starts

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,10 @@
+def SessionsController<ApplicationController
+    def new
+    end
+
+    def create
+    end
+
+    def destroy
+    end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,11 @@ class UsersController<ApplicationController
     
     def show
         @user=User.find(params[:id])
-         @articles =@user.articles
+         @articles =@user.articles.paginate(page: params[:page], per_page: 2)
+    end
+    
+    def index
+        @users=User.paginate(page: params[:page], per_page: 2)
     end
 
     def edit
@@ -26,7 +30,7 @@ class UsersController<ApplicationController
         @user=User.find(params[:id])
         if  @user.update(user_params)
             flash[:notice]="User Information was updated succesully"
-            redirect_to articles_path
+            redirect_to @user
         else
             render 'edit'
         end
